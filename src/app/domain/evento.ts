8dd548591d944b623678bc4486ef522d517ac69f
Renato Cesar do Nascimento R. Caetano <renato.caetano@usr.brflt.corp>
2017-09-13 16:54:10 -0300

    modificacao no card html

---
+++ src/app/domain/evento.ts
@@ -0,0 +1,20 @@
+export class Evento {
+
+    public id: string;
+    public titulo: string;
+    public descricao: string;
+    public foto: string;
+    public dia: string;
+    public mes: string;
+    public ano: string;
+
+    /*
+    public hora: string;
+    public local: string;
+    public cidade: string;
+    public bairro: string;
+    public rua: string;
+    public numero: string;
+    */
+
+}
